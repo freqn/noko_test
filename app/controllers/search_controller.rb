ca2e4a719fd63f9248d2989552bf85f9b81ee309
c <freqnlabs@gmail.com>
2014-08-07 21:57:11 -0400

    update default query

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
 
   def index
-    params[:q] = 'rails' if params[:q].blank?
+    params[:q] = 'ruby on rails' if params[:q].blank?
     @reddit_query = params[:q].gsub(/\s/,'')
     @fetch_reddit = RedditScraper.new.fetch_reddit_headlines(@reddit_query)
     @fetch_hn = HnScraper.new.fetch_hn_headlines(params[:q])
@@ -10,7 +10,7 @@ class SearchController < ApplicationController
   end
 
   def tweet_links
-    params[:q] = 'rails' if params[:q].blank?
+    params[:q] = 'ruby on rails' if params[:q].blank?
     @tweet_query = params[:q].gsub(/\s/,"%20").downcase
     @tweets = []
     @client.search(params[:q]).take(70).each do |tweet|
